d0778535d5a7c1dd503799c5f71fe24e6e7259a9
谢志凯 <xiezhikai@appledeimac.local>
2016-09-06 10:14:27 +0800

    Initial Commit

---
+++ EveryDayComplete/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EveryDayComplete
+//
+//  Created by 谢志凯 on 16/9/6.
+//  Copyright © 2016年 杨川. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EveryDayComplete/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  EveryDayComplete
+//
+//  Created by 谢志凯 on 16/9/6.
+//  Copyright © 2016年 杨川. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
